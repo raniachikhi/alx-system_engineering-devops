4a9ce73eea70a0c5b8cc320e2a1777f679379cb8
raniachikhi <raniach0653@gmail.com>
2023-09-27 08:42:34 -0700

    add

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,14 @@
-#!/usr/bin//env bash
+#!/usr/bin/env bash
 # you can connect to a server without typing a password.
+
 file_line { 'Turn off passwd auth':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
   line   => '    PasswordAuthentication no',
-  }
+}
+
 file_line { 'Declare identity file':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
-  line   => '    IdentityFile ~/.ssh/school,
-  }
+  line   => '    IdentityFile ~/.ssh/school',
+}
