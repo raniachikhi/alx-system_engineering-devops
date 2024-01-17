0a9d6a8423b219377f6f193f3d400ecb56699912
raniachikhi <raniach0653@gmail.com>
2024-01-16 16:05:38 -0800

    add

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,6 +4,7 @@
 import requests
 
 def number_of_subscribers(subreddit):
+    """ number of subscribers """
     api_url = f"https://www.reddit.com/r/{subreddit}/about.json"
 
     headers = {'User-Agent': 'MyRedditBot/1.0'}
